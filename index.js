5642b5e0635a201ad15ee22a1a9178aa41301805
RayFlaherty <raymondf.flaherty@gmail.com>
2021-12-04 20:19:42 -0600

    some node fuctionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { prompt } = require('inquirer');
 const inquirer = require('inquirer');
 
 
@@ -21,4 +22,52 @@ const questions= () => {
         }
     ]);
 };
-questions().then (answers => console.log(answers));
+//questions().then (answers => console.log(answers));
+
+const readmeInfo = () => {
+    console.log(`
+        ===============================================
+                 Your ReadMe starts now!
+        Plan ahead, your ReadMe should have a nice 
+        table of contents. Think of how you want the 
+        layout to look. We are going to add prompts for
+        1. Installation guide, 2. Usage guide, 3. Credits
+        for the project, 4. Licensing. Fill out the 
+        following to get started. 
+        ===============================================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project name',
+            message: 'What is your project name?'
+        },
+        {
+            type:'input',
+            name:'install',
+            message:'What programs need to be installed for your project to operate?'
+
+        },
+        {
+            type:'input',
+            name:'guide',
+            message:'How do you use your project'
+        },
+        {
+            type:'input',
+            name:'credits',
+            message:'Who is involved in the project'
+        },
+        {
+            type:'input',
+            name:'licensing',
+            message:'Security licensing'
+        }
+    ]);
+};
+
+
+questions()
+     .then (questions => console.log(questions))
+     .then (readmeInfo)
+     .then (readmeInfo => console.log (readmeInfo));
