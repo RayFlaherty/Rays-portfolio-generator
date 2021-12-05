e19681aa58e115c20a4b96fcda3659291aadb7da
RayFlaherty <raymondf.flaherty@gmail.com>
2021-12-04 19:10:56 -0600

    beginning text

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Rays-portfolio-generator
 
 For this project I am going to create a program using Node in order to generate a readme file by using the command-line application. I will add a video file of the use of the final project.
+
+Installation guide.(Be sure to add why and try and add screeshots)
+
+Install Node.js
+    npm init
+
+Install Inquirer
+    npm install inquirer
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('am i working')
+const inquirer = require('inquirer');
+
+
+inquirer
+const questions= () => {
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message:'What is your name?',
+        },
+        {
+            type:'input',
+            name:'github',
+            message:'What is your GitHub name?',
+        },
+        {
+            type:'input',
+            name:'reason',
+            message:'What is the purpose of your readme?'
+        }
+    ]);
+};
+questions().then (answers => console.log(answers));
